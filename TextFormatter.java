c579975d63f9661f7008c5a95e7d0d71f930038a
repl.it user <replituser@example.com>
2020-10-31 17:30:56 +0000

    Added center and right padding

--- TextFormatter.java
+++ TextFormatter.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 class TextFormatter {
   // Attributes
   private final int maxLineLength;
-  private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
+  private static final String text = "Lorem ipsum dolor sit amet, consetetur                  sadipscing elitr, sed diam nonumy " +
           "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
           "accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem " +
           "ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod " +
@@ -13,7 +13,11 @@ class TextFormatter {
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
-    formatter.print(text);
+    formatter.print(text, 'c');
+    System.out.print("\n");
+    formatter.print(text, 'l');
+    System.out.print("\n");
+    formatter.print(text, 'r');
   }
 
   //Konstruktor
@@ -21,11 +25,32 @@ class TextFormatter {
     this.maxLineLength = maxLineLength;
   }
 
-  // Ausgabe
-  public void print(String aText) {
+  // Print Function takes a String and an Argument char which desides the 
+  // alinement of the output (c = center, l = left, r = right)
+  public void print(String aText, char arg) {
     String line = "";
     StringTokenizer st = new StringTokenizer(aText);
-    
+
+    switch(arg)
+    {
+      case 'l':
+        output(line, st, arg);
+        break;
+      case 'r':
+        output(line, st, arg);
+        break;
+      case 'c':
+        output(line, st, arg); 
+        break;
+      default:
+        System.out.print("Error, wrong padding argument");
+        break;
+
+    }
+  }
+
+  public void output(String line, StringTokenizer st, char arg)
+  {
     while(st.hasMoreTokens())
     {
       String word = st.nextToken();
@@ -39,19 +64,61 @@ class TextFormatter {
       }
 
       else{
-        System.out.print(line);
-        System.out.print("\n");
+        if(arg != 'l')
+        {
+          line = paddLine(line, arg);
+        }
+        System.out.print(line + '\n');
         line = "";
+        line += word + ' ';
       }
     }
     // Prints out the last line of the given String 
+    if(arg != 'l')
+    {
+      line = paddLastLine(line, arg);
+    }
     System.out.print(line + '\n');
+  }
+
+  public String paddLine(String line, char arg)
+  {
+    String temp = "";
+    int diff = maxLineLength - line.length();
+    if(arg == 'c')
+    {
+      diff = diff/2;
+    }
+    for (int i = 0; i<= diff; i++)
+      {
+        temp += ' ';
+      }
+    temp += line;
+    line = temp;
+    return line;
+  }
+
+  public String paddLastLine(String line, char arg)
+  {
+    String temp = "";
+    int diff = maxLineLength - line.length();
+    if(arg == 'c')
+    {
+      diff = diff/2;
+    }
+    for(int i =  0; i<= diff;i++ )
+    {
+      temp += ' ';
+    }
+    temp += line;
+    line = temp;
+    return line;
   }
   
   public String cutWord(String word){
-    String firstPart = word.substring(0, maxLineLength);
-    System.out.print(firstPart + "\n");
-    String lastPart = word.substring(maxLineLength) + " ";
+    String firstPart = word.substring(0, maxLineLength - 1);
+    System.out.print(firstPart + "-\n");
+    String lastPart = word.substring(maxLineLength - 1) + " ";
     return lastPart;
   } 
 }
